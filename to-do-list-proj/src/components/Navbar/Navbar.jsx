1f19e0b08fe467a64c9ce2fb154d47180b226820
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2023-01-22 17:54:45 -0400

    add navbar

---
+++ to-do-list-proj/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+import logoBlue from "../../assets/images/logoblue.gif"
+
+function Navbar() {
+
+    return(
+        <div className="Navbar">
+            <h1 className="Title">To Do List</h1>
+            <img src={logoBlue} alt="Logo da Blue EdTech" />
+        </div>
+    );
+}
+
+export default Navbar;
--- to-do-list-proj/src/components/View/Home.jsx
+++ to-do-list-proj/src/components/View/Home.jsx
@@ -1,1 +1,13 @@
 import "./Home.css";
+import Navbar from "../navbar/Navbar";
+
+function Home() {
+
+    return (
+        <div >
+            <Navbar />
+        </div>
+    );
+}
+
+export default Home;
