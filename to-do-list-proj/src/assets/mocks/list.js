b4e872bbbcc36b952b57f229980e12c7fb418518
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2023-01-22 18:55:42 -0400

    add mock

---
+++ to-do-list-proj/src/assets/mocks/list.js
@@ -0,0 +1,7 @@
+export const list = [
+    {
+        id: 1,
+        text: "afazer de exemplo",
+        complete: false
+    }
+]
--- to-do-list-proj/src/components/ToDoList/ToDoList.jsx
+++ to-do-list-proj/src/components/ToDoList/ToDoList.jsx
@@ -2,18 +2,23 @@ import "./ToDoList.css";
 
 function ToDoList() {
     return (
-        <form>
-            <input
-                type="text"
-                id="new-todo-input"
-                className="input input__lg"
-                name="text"
-                autoComplete="off"
-            />
-            <button type="submit" className="btn btn__add">
-                Adicionar
-            </button>
-        </form>
+        <div>
+            <form>
+                <input
+                    type="text"
+                    id="add-input"
+                    className="input input__add"
+                    name="text"
+                    autoComplete="off"
+                />
+                <button type="submit" className="btn btn__add">
+                    Adicionar
+                </button>
+            </form>
+            <ul>
+                
+            </ul>
+        </div>
     );
 }
 
