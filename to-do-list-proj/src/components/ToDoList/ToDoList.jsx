d3b0b1c423a1691dce54756c97650aead2f6a54e
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2023-01-22 22:52:23 -0400

    add addTarefa

--- to-do-list-proj/src/components/ToDoList/ToDoList.jsx
+++ to-do-list-proj/src/components/ToDoList/ToDoList.jsx
@@ -1,15 +1,24 @@
 import "./ToDoList.css";
 import { useState } from "react";
-import {list} from "../../assets/mocks/list";
+// import { list } from "../../assets/mocks/list";
 import ToDoForm from "../ToDoForm/ToDoForm";
 
 function ToDoList() {
-    const [tarefa, setTarefa] = useState([]);
+    const [tarefas, setTarefas] = useState([]);
 
+    const addTarefa = (tarefa) => {
+        if (!tarefa.text) {
+            return;
+        }
+
+        const novaTarefa = [tarefa, ...tarefas];
+
+        setTarefas(novaTarefa);
+    };
 
     return (
         <div>
-            <ToDoForm />
+            <ToDoForm onSubmit={addTarefa} />
         </div>
     );
 }
